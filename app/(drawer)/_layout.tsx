46ea4e7959732e7d2b16bf7121d884862a92ddff
noguerajulioces <noguerajulio24@gmail.com>
2024-12-12 14:03:52 -0300

    update

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -12,37 +12,46 @@ const CustomDrawerContent = (props: any) => {
   const appVersion = Application.nativeApplicationVersion;
 
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItem
-        label={t('commonScreen.drawer.removeAds')}
-        icon={() => <FontAwesome name="ban" size={25} color="#000" /> }
-        onPress={() => {
-          router.push('/remove-ads')
-        }}
-      />
-      <DrawerItem
-        label={t('commonScreen.drawer.language')}
-        icon={() => <FontAwesome name="language" size={25} color="#000" /> }
-        onPress={() => {
-          router.push('/language')
-        }}
-      />
-      <DrawerItem
-        label={t('commonScreen.drawer.sharedAndWin')}
-        icon={() => <FontAwesome name="share" size={25} color="#000" /> }
-        onPress={() => {
-          router.push('/(drawer)/(tabs)/loans')
-        }}
-      />
-      <DrawerItem
-        label={t('commonScreen.drawer.rateUs')}
-        icon={() => <FontAwesome name="star" size={25} color="#000" /> }
-        onPress={() => {
-          router.push('/(drawer)/(tabs)/loans')
-        }}
-      />
-
+    <DrawerContentScrollView 
+        {...props}
+        contentContainerStyle={styles.scrollContent}
+      >
+      <View style={styles.drawerItems}>
+        <DrawerItem
+          label={t('commonScreen.drawer.removeAds')}
+          icon={() => <FontAwesome name="ban" size={25} color="#000" /> }
+          onPress={() => {
+            router.push('/remove-ads')
+          }}
+        />
+        <DrawerItem
+          label={t('commonScreen.drawer.language')}
+          icon={() => <FontAwesome name="language" size={25} color="#000" /> }
+          onPress={() => {
+            router.push('/language')
+          }}
+        />
+        <DrawerItem
+          label={t('commonScreen.drawer.sharedAndWin')}
+          icon={() => <FontAwesome name="share" size={25} color="#000" /> }
+          onPress={() => {
+            router.push('/(drawer)/(tabs)/loans')
+          }}
+        />
+        <DrawerItem
+          label={t('commonScreen.drawer.rateUs')}
+          icon={() => <FontAwesome name="star" size={25} color="#000" /> }
+          onPress={() => {
+            router.push('/(drawer)/(tabs)/loans')
+          }}
+        />
+      </View>
       <View style={styles.versionContainer}>
+        <View style={ styles.socialBox }>
+          <Text>
+            Social Media
+          </Text>
+        </View>
         <Text style={styles.versionText}>{`Version: ${appVersion}`}</Text>
       </View>
     </DrawerContentScrollView>
@@ -50,6 +59,25 @@ const CustomDrawerContent = (props: any) => {
 };
 
 const styles = StyleSheet.create({
+  socialBox: {
+    backgroundColor: '#3445FD', 
+    paddingTop: 180, 
+    marginBottom: 15, 
+    width: '100%', 
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 4, height: 6 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  scrollContent: {
+    flexGrow: 1,
+    justifyContent: 'space-between'
+  },
+  drawerItems: {
+    flex: 1,
+  },
   versionContainer: {
     marginTop: 20,
     alignItems: 'center',
