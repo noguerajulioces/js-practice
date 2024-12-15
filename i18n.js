199bed9d721f0e6f59fb198f4c964155d2393291
noguerajulioces <noguerajulio24@gmail.com>
2024-12-15 17:29:52 -0300

    add locales

--- i18n.js
+++ i18n.js
@@ -7,6 +7,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import es from './locales/es/translation.json';
 import en from './locales/en/translation.json';
 import fr from './locales/fr/translation.json';
+import ja from '@/locales/ja/translation.json';
+import ru from '@/locales/ru/translation.json';
+import it from '@/locales/it/translation.json';
+import de from '@/locales/de/translation.json';
+import hi from '@/locales/hi/translation.json';
+import zh from '@/locales/zh/translation.json';
+import pt from '@/locales/pt/translation.json';
 
 const fallbackLng = 'en';
 const deviceLanguage = Localization.locale.split('-')[0];
@@ -22,6 +29,13 @@ const deviceLanguage = Localization.locale.split('-')[0];
         es: { translation: es },
         en: { translation: en },
         fr: { translation: fr },
+        ja: { translation: ja },
+        ru: { translation: ru },
+        it: { translation: it },
+        de: { translation: de },
+        hi: { translation: hi },
+        zh: { translation: zh },
+        pt: { translation: pt }
       },
       lng: defaultLanguage,
       fallbackLng: deviceLanguage || fallbackLng,
